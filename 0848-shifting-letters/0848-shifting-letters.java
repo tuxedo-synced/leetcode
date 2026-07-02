dfb3c55a92b69f2258e51cc21ca549768f1e0979
SADARAM RAHUL <rahul.synced@gmail.com>
2026-07-02 17:52:06 +0530

    Added new approach in comments

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -14,3 +14,23 @@ class Solution {
         return ans ;
     }
 }
+
+/*
+class Solution {
+    public String shiftingLetters(String s, int[] shifts) {
+        
+        char[] arr = s.toCharArray();
+
+        long total = 0;
+
+        for (int i = shifts.length - 1; i >= 0; i--) {
+
+            total = (total + shifts[i]) % 26;
+
+            arr[i] = (char)((arr[i] - 'a' + total) % 26 + 'a');
+        }
+
+        return new String(arr);
+    }
+}
+*/
