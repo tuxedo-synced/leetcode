c3e2dc54a4d5d7ba1f5d350a1b17dc9d5dc87f81
SADARAM RAHUL <rahul.synced@gmail.com>
2026-07-12 18:54:10 +0530

    Time: 32 ms (78.96%), Space: 60.3 MB (9.99%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -26,3 +26,38 @@ class Solution {
         return ans ;
     }
 }
+
+/*
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+
+            HashSet<Integer> seen = new HashSet<>();
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                int target = -nums[i] - nums[j];
+
+                if (seen.contains(target)) {
+                    ans.add(Arrays.asList(nums[i], target, nums[j]));
+
+                    while (j + 1 < nums.length && nums[j] == nums[j + 1])
+                        j++;
+                }
+
+                seen.add(nums[j]);
+            }
+        }
+
+        return ans;
+    }
+}
+
+*/
