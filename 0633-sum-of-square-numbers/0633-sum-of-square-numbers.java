f0a8364036cdf65979dbbb7b11b38510153c6a6f
SADARAM RAHUL <rahul.synced@gmail.com>
2026-07-16 19:22:09 +0530

    Time: 3 ms (96.57%), Space: 41.7 MB (98.62%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -11,3 +11,19 @@ class Solution {
         return false;
     }
 }
+
+/*
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        for (long i = 0; i * i <= c; i++) {
+            long rem = c - i * i;
+            long root = (long) Math.sqrt(rem);
+
+            if (root * root == rem) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+*/
