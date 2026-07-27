39ce6a250cf1df3bbdd3201cb79ca09441627687
SADARAM RAHUL <rahul.synced@gmail.com>
2026-07-27 20:15:37 +0530

    Time: 1 ms (25.27%), Space: 52.7 MB (78.19%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,5 +1,17 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        int i = 0 ;
+        while(i < nums.length-1){
+            if(nums[i] != nums[i+1]) return nums[i];
+            i+=2;
+        }
+        return nums[nums.length-1];
+    }
+}
+
+/*
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
         int ans = 0;
         for(int i : nums){
             ans^=i;
@@ -7,3 +19,4 @@ class Solution {
         return ans ;
     }
 }
+*/
